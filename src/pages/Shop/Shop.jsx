3efcb28cebdfd74467c15778555554d90a167c14
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-03-12 12:10:25 +0600

    load data base pagination

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -108,7 +108,7 @@ export default function Shop() {
                     onChange={event => setSize(event.target.value)}
                 >
                     <option value="5">5</option>
-                    <option value="10">10</option>
+                    <option value="10" selected>10</option>
                     <option value="15">15</option>
                     <option value="20">20</option>
                 </select>
